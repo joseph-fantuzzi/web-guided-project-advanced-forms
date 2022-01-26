0998899dfa702d1e791b11a7dc1ac74b38899ba2
Casey Harding <casey.r.harding@gmail.com>
2022-01-26 14:06:16 -0500

    Final solution code in place.

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,9 @@ import FriendForm from './FriendForm'
 // 🔥 STEP 2- FLESH OUT FriendForm.js
 // 🔥 STEP 3- FLESH THE SCHEMA IN ITS OWN FILE
 // 🔥 STEP 4- IMPORT THE SCHEMA, AXIOS AND YUP
+import axios from 'axios';
+import * as yup from 'yup';
+import schema from '../validation/formSchema';
 
 
 //////////////// INITIAL STATES ////////////////
@@ -48,19 +51,43 @@ export default function App() {
   const getFriends = () => {
     // 🔥 STEP 5- IMPLEMENT! ON SUCCESS PUT FRIENDS IN STATE
     //    helper to [GET] all friends from `http://buddies.com/api/friends`
+    axios.get('http://buddies.com/api/friends')
+      .then(res => {
+        setFriends(res.data);
+      }).catch(err => console.error(err))
   }
 
   const postNewFriend = newFriend => {
     // 🔥 STEP 6- IMPLEMENT! ON SUCCESS ADD NEWLY CREATED FRIEND TO STATE
     //    helper to [POST] `newFriend` to `http://buddies.com/api/friends`
     //    and regardless of success or failure, the form should reset
+    axios.post('http://buddies.com/api/friends', newFriend)
+      .then(res => {
+        setFriends([res.data, ...friends])
+        setFormValues(initialFormValues);
+      }).catch(err => console.error(err))
   }
 
+  const validate = (name, value) => {
+    yup.reach(schema, name)
+      .validate(value)
+      .then(() => setFormErrors({ ...formErrors, [name]: '' }))
+      .catch(err => setFormErrors({ ...formErrors, [name]: err.errors[0] }))
+  }
+
+  // const yup = {
+  //   reach: function
+  // }
+
+  // const axios = {
+  //   post: function()
+  // }
   //////////////// EVENT HANDLERS ////////////////
   //////////////// EVENT HANDLERS ////////////////
   //////////////// EVENT HANDLERS ////////////////
   const inputChange = (name, value) => {
     // 🔥 STEP 10- RUN VALIDATION WITH YUP
+    validate(name, value);
     setFormValues({
       ...formValues,
       [name]: value // NOT AN ARRAY
@@ -74,9 +101,34 @@ export default function App() {
       role: formValues.role.trim(),
       civil: formValues.civil.trim(),
       // 🔥 STEP 7- WHAT ABOUT HOBBIES?
+      hobbies: ['hiking', 'reading', 'coding'].filter(hobby => !!formValues[hobby])
     }
     // 🔥 STEP 8- POST NEW FRIEND USING HELPER
+    postNewFriend(newFriend);
   }
+/**
+ * How do you access properties of an object?!
+ * dot notation => you have to know the specific key
+ * bracket notation => you can use a variable!
+ */
+
+// const lorem = {
+//   name: "Casey",
+//   age: 74,
+//   occupation: raccoon
+// }
+
+// const toUpdate = "name";
+
+// lorem[toUpdate] = "Harding"
+  // !false => true
+  // !"Casey" => !false => true
+  // !!"Casey" => true
+  // !null => !true => false
+  // !1 => !false => true
+  // !{} => !false => true
+  // !![] => true
+  // !!-1000000 => true
 
   //////////////// SIDE EFFECTS ////////////////
   //////////////// SIDE EFFECTS ////////////////
@@ -87,7 +139,8 @@ export default function App() {
 
   useEffect(() => {
     // 🔥 STEP 9- ADJUST THE STATUS OF `disabled` EVERY TIME `formValues` CHANGES
-  }, [])
+    schema.isValid(formValues).then(valid => setDisabled(!valid))
+  }, [formValues])
 
   return (
     <div className='container'>
